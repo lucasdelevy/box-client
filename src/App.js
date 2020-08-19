90816d2e732c8bcd07178df2528e0124e462005b
Lucas de Levy Oliveira <oliluca@amazon.com>
2020-08-19 10:54:49 -0300

    Add authentication form with GraphQL call

--- src/App.js
+++ src/App.js
@@ -1,24 +1,25 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import Header from './components/Header/Header';
+import RegistrationForm from "./components/RegistrationForm/RegistrationForm";
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route
+} from "react-router-dom";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Header />
+      <div className="container d-flex align-items-center flex-column">
+        <Router>
+          <Switch>
+            <Route path="/" exact={true}>
+              <RegistrationForm />
+            </Route>
+          </Switch>
+        </Router>
+      </div>
     </div>
   );
 }
